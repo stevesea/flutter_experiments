e1bd23c61c9800d6bd44e3fad451080c73640dd7
Steve Christensen <stevec1973@gmail.com>
2017-09-19 00:00:09 -0600

    advsmith2

---
+++ advsmith2/android/app/src/main/kotlin/org/stevesea/adventuresmith2/advsmith2/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.stevesea.adventuresmith2.advsmith2
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
