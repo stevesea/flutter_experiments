fa1effd4f084e9e41ff7a0fb4bbe11f4bc884602
Steve Christensen <stevec1973@gmail.com>
2017-11-26 02:45:59 -0700

    add trims back in

--- advsmith2/lib/dice_parser.dart
+++ advsmith2/lib/dice_parser.dart
@@ -14,8 +14,8 @@ class DiceParser {
       ..primitive(char('(').trim().seq(root).seq(char(')').trim()).pick(1))
       ..primitive(digit().plus().flatten().trim().map((a) => int.parse(a)));
     builder.group()
-      ..postfix(string('dF'), action((a, op) => roller.rollFudge(a)))
-      ..left(char('d'), action((a, op, b) => roller.roll(a, b)));
+      ..postfix(string('dF').trim(), action((a, op) => roller.rollFudge(a)))
+      ..left(char('d').trim(), action((a, op, b) => roller.roll(a, b)));
     builder.group()
       ..left(char('*').trim(), action((a, op, b) => a * b));
     builder.group()
